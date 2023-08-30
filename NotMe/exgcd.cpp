1a5b21aed728ed6ca421f600bb456e0ef4f5bd1b
asxziill <106165027+asxziill@users.noreply.github.com>
2023-08-30 15:57:21 +0800

    Add files via upload

---
+++ NotMe/exgcd.cpp
@@ -0,0 +1,11 @@
+using i64 = long long;
+ 
+i64 exgcd(i64 a, i64 b, i64 &x, i64 &y) {
+    if (b == 0) {
+        x = 1, y = 0;
+        return a;
+    }
+    i64 g = exgcd(b, a % b, y, x);
+    y -= a / b * x;
+    return g;
+}
